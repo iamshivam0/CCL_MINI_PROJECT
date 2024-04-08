99a07917503bb2adb6103f4e0464a9e7e603cec0
Shivam <shivamchandak9@gmail.com>
2024-04-08 14:27:37 +0530

    all origns

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://abc2-xi.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
